57231fbb3029ff1c937b5d5d8c04ba5a8f17d203
TheBi11iona1re <78707011+TheBi11iona1re@users.noreply.github.com>
2024-04-15 15:57:14 +1000

    wow more stuff blur and titlebar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+  </script>
+  
+
+
+  <div class="absolute top-0 h-8 w-full" data-tauri-drag-region>
+
+  </div>
+  <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<head>
+    <style>
+      html,
+      body {
+        background: transparent;
+      }
+    </style>
+  </head>
+
+<h1 class="font-bold">Welcome to SvelteKit & tailwind best</h1>
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
+
+  <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    Button
+    </button>
+
+  
